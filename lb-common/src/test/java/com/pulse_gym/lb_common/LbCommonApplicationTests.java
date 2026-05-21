6e4ba45fd08db6a8691338ebd6f064d14f98db5d
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-20 22:10:41 -0500

    feat: config de encriptacion, base para lbreria en comun

---
+++ lb-common/src/test/java/com/pulse_gym/lb_common/LbCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse_gym.lb_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
