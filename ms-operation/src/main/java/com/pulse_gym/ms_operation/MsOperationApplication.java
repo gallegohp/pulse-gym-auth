fd373777fc4b376840eedc16b3daac0f6a67dfae
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-20 20:48:38 -0500

    feat: se dockeriza el ms-operation

---
+++ ms-operation/src/main/java/com/pulse_gym/ms_operation/MsOperationApplication.java
@@ -0,0 +1,13 @@
+package com.pulse_gym.ms_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperationApplication.class, args);
+	}
+
+}
