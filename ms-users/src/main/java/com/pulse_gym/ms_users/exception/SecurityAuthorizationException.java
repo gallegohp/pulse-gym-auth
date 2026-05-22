1db43c5f4d3fcf5ea8756cbe207b35e145e20811
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-05-22 03:54:32 -0500

    refactor: Se agregar la validacion de roles y con correciones en otros ms 'lb-common' en jwtservice - 'ms-apigatwway' el filtro

---
+++ ms-users/src/main/java/com/pulse_gym/ms_users/exception/SecurityAuthorizationException.java
@@ -0,0 +1,8 @@
+package com.pulse_gym.ms_users.exception;
+
+public class SecurityAuthorizationException extends RuntimeException {
+
+    public SecurityAuthorizationException(String message) {
+        super(message);
+    }
+}
