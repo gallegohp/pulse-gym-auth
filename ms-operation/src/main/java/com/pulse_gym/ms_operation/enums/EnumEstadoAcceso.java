01d582f3d41e598ac31490d142a2d4b6593837c6
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-22 08:30:02 -0500

    refactor: se modifica la sintaxis de manejo de enums

--- ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumEstadoAcceso.java
+++ ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumEstadoAcceso.java
@@ -2,28 +2,8 @@ package com.pulse_gym.ms_operation.enums;
 
 public enum EnumEstadoAcceso {
 
-    PERMITIDO(1L),
-    DENEGADO(2L);
+    PERMITIDO,
+    DENEGADO;
 
-    private final Long id;
-
-    EnumEstadoAcceso(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public static EnumEstadoAcceso fromId(Long id) {
-
-        for (EnumEstadoAcceso estado : values()) {
-
-            if (estado.getId().equals(id)) {
-                return estado;
-            }
-        }
-
-        throw new IllegalArgumentException("Estado de acceso no válido: " + id);
-    }
+    
 }
--- ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumTipoAcceso.java
+++ ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumTipoAcceso.java
@@ -2,29 +2,8 @@ package com.pulse_gym.ms_operation.enums;
 
 public enum EnumTipoAcceso {
 
-    HUELLA(1L),
-    WEB(2L),
-    APP(3L);
+    HUELLA,
+    WEB,
+    APP;
 
-    private final Long id;
-
-    EnumTipoAcceso(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public static EnumTipoAcceso fromId(Long id) {
-
-        for (EnumTipoAcceso tipo : values()) {
-
-            if (tipo.getId().equals(id)) {
-                return tipo;
-            }
-        }
-
-        throw new IllegalArgumentException("Tipo de acceso no válido: " + id);
-    }
 }
