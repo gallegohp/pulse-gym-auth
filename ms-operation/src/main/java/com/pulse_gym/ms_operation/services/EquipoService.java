13ea67ce79ccbe82869a90a62e7f85f460d8d2b7
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-22 13:14:07 -0500

    fix: correcion EquipoService, error de MessegeGlobalDTO

--- ms-operation/src/main/java/com/pulse_gym/ms_operation/services/EquipoService.java
+++ ms-operation/src/main/java/com/pulse_gym/ms_operation/services/EquipoService.java
@@ -17,57 +17,54 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EquipoService {
 
-    private final EquipoRepository equipoRepository;
-    private final ProveedorRepository proveedorRepository;
-    private final SedeRepository sedeRepository;
+        private final EquipoRepository equipoRepository;
+        private final ProveedorRepository proveedorRepository;
+        private final SedeRepository sedeRepository;
 
-    MessegeGlobalDTO response = new MessegeGlobalDTO();
 
-    public MessegeGlobalDTO registrarEquipo(EquipoRequestDTO equipoRequestDTO) {
+        public MessegeGlobalDTO registrarEquipo(EquipoRequestDTO equipoRequestDTO) {
 
-        if (equipoRepository
-                .findByNumeroSerie(equipoRequestDTO.getNumeroSerie())
-                .isPresent()) {
-            
-            response.setMessage("El numero de serie ya esta en uso");
-            return response;
+                if (equipoRepository
+                                .findByNumeroSerie(equipoRequestDTO.getNumeroSerie())
+                                .isPresent()) {
+
+                        MessegeGlobalDTO response = new MessegeGlobalDTO("El número de serie ya existe");
+                        return response;
+                }
+
+                Proveedor proveedor = proveedorRepository
+                                .findById(equipoRequestDTO.getIdProveedor())
+                                .orElseThrow(() -> new RuntimeException("Proveedor no encontrado"));
+
+                Sede sede = sedeRepository
+                                .findById(equipoRequestDTO.getIdSede())
+                                .orElseThrow(() -> new RuntimeException("Sede no encontrada"));
+
+                Equipo equipo = new Equipo();
+
+                equipo.setProveedor(proveedor);
+                equipo.setSede(sede);
+
+                equipo.setNombre(equipoRequestDTO.getNombre());
+                equipo.setMarca(equipoRequestDTO.getMarca());
+                equipo.setModelo(equipoRequestDTO.getModelo());
+                equipo.setNumeroSerie(equipoRequestDTO.getNumeroSerie());
+
+                equipo.setFechaAdquisicion(
+                                equipoRequestDTO.getFechaAdquisicion());
+
+                equipo.setFechaGarantia(
+                                equipoRequestDTO.getFechaGarantia());
+
+                equipo.setUbicacion(
+                                equipoRequestDTO.getUbicacion());
+
+                equipo.setEstado(
+                                equipoRequestDTO.getEstado());
+
+                equipoRepository.save(equipo);
+
+                MessegeGlobalDTO response = new MessegeGlobalDTO("Equipo registrado correctamente");
+                return response;
         }
-
-        Proveedor proveedor = proveedorRepository
-                .findById(equipoRequestDTO.getIdProveedor())
-                .orElseThrow(() ->
-                        new RuntimeException("Proveedor no encontrado"));
-
-        Sede sede = sedeRepository
-                .findById(equipoRequestDTO.getIdSede())
-                .orElseThrow(() ->
-                        new RuntimeException("Sede no encontrada"));
-
-        Equipo equipo = new Equipo();
-
-        equipo.setProveedor(proveedor);
-        equipo.setSede(sede);
-
-        equipo.setNombre(equipoRequestDTO.getNombre());
-        equipo.setMarca(equipoRequestDTO.getMarca());
-        equipo.setModelo(equipoRequestDTO.getModelo());
-        equipo.setNumeroSerie(equipoRequestDTO.getNumeroSerie());
-
-        equipo.setFechaAdquisicion(
-                equipoRequestDTO.getFechaAdquisicion());
-
-        equipo.setFechaGarantia(
-                equipoRequestDTO.getFechaGarantia());
-
-        equipo.setUbicacion(
-                equipoRequestDTO.getUbicacion());
-
-        equipo.setEstado(
-                equipoRequestDTO.getEstado());
-
-        equipoRepository.save(equipo);
-
-        response.setMessage("Equipo registrado correctamente");
-        return response;
-    }
 }
