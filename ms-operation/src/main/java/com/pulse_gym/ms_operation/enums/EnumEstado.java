7888c934fc22245306ab5ed1a3cf943f2207bfec
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-22 08:30:02 -0500

    refactor: se modifica la sintaxis de manejo de enums

--- ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumEstado.java
+++ ms-operation/src/main/java/com/pulse_gym/ms_operation/enums/EnumEstado.java
@@ -2,30 +2,10 @@ package com.pulse_gym.ms_operation.enums;
 
 public enum EnumEstado {
 
-    OPERATIVO(1L),
-    MANTENIMIENTO(2L),
-    FUERA_DE_SERVICIO(3L),
-    RETIRADO(4L);
+    OPERATIVO,
+    MANTENIMIENTO,
+    FUERA_DE_SERVICIO,
+    RETIRADO;
 
-    private final Long id;
-
-    EnumEstado(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public static EnumEstado fromId(Long id) {
-
-        for (EnumEstado estado : values()) {
-
-            if (estado.getId().equals(id)) {
-                return estado;
-            }
-        }
-
-        throw new IllegalArgumentException("Estado no válido: " + id);
-    }
+    
 }
