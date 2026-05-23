a4e6011f61e7e48eabe51ba8c16990e6b6d65060
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-23 09:33:30 -0500

    feat: se crea endpoint /consultar, buscar equipos

--- pg-ms-operation/src/main/java/com/pulse_gym/ms_operation/repository/EquipoRepository.java
+++ pg-ms-operation/src/main/java/com/pulse_gym/ms_operation/repository/EquipoRepository.java
@@ -3,12 +3,15 @@ package com.pulse_gym.ms_operation.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.pulse_gym.lb_common.entity.operation.Equipo;
 
 @Repository
-public interface EquipoRepository extends JpaRepository<Equipo, Long> {
+public interface EquipoRepository extends JpaRepository<Equipo, Long>, 
+                                        JpaSpecificationExecutor<Equipo> {
+    
     Optional<Equipo> findByNumeroSerie(String numeroSerie);
     
 } 
