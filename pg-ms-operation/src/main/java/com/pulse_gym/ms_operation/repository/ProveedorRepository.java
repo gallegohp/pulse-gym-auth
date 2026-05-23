055369e10bf774c2b7e1092ff05b60e6f28a63e9
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-05-22 23:51:39 -0500

    refactor: reorganizar entidades por contexto y eliminar repositorios compartidos

--- pg-ms-operation/src/main/java/com/pulse_gym/ms_operation/repository/ProveedorRepository.java
+++ pg-ms-operation/src/main/java/com/pulse_gym/ms_operation/repository/ProveedorRepository.java
@@ -1,9 +1,9 @@
-package com.pulse_gym.lb_common.repository;
+package com.pulse_gym.ms_operation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pulse_gym.lb_common.entity.Proveedor;
+import com.pulse_gym.lb_common.entity.operation.Proveedor;
 
 @Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
--- pg-ms-users/src/main/java/com/pulse_gym/ms_users/MsUsersApplication.java
+++ pg-ms-users/src/main/java/com/pulse_gym/ms_users/MsUsersApplication.java
@@ -2,8 +2,11 @@ package com.pulse_gym.ms_users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"com.pulse_gym.ms_users", "com.pulse_gym.lb_common"})
+@SpringBootApplication(scanBasePackages = "com.pulse_gym")
+@EntityScan("com.pulse_gym.lb_common.entity.user")
+
 public class MsUsersApplication {
 
     public static void main(String[] args) {
