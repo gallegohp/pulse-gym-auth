fb379a4c96502759462539d2b8e9d6ecdfc592fa
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-29 10:25:59 -0500

    PLANTILLACONTROLLER feat: se crea endpoint de inavilirar estado de la plantilla

--- pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
+++ pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -59,6 +61,11 @@ public class PlantillaController {
         }
     }
 
+    /**
+     * Consulta las plantillas de notificaciones
+     * @param userRol Rol del usuario
+     * @return ResponseEntity<Map<String, Object>> con el resultado de la operación
+     */
     @GetMapping("/leer")
     public ResponseEntity<Map<String, Object>> leerPlantilla(@RequestHeader(value = "X-User-Rol", required = false) String userRol) {
         try {
@@ -71,6 +78,25 @@ public class PlantillaController {
             response.put("data", notificaciones);
             
             return ResponseEntity.ok(response);
+            
+        } catch (Exception e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+
+    @PostMapping("/inaviliar/{id}")
+    public ResponseEntity<Map<String, Object>> inactivarPlantilla(@PathVariable Long id, @RequestHeader(value = "X-User-Rol", required = false) String userRol) {
+        try {
+            MessegeGlobalDTO response = plantillaService.inactivarPlantilla(id, userRol);
+            
+            Map<String, Object> respuesta = new HashMap<>();
+            respuesta.put("success", true);
+            respuesta.put("message", response.getMessage());
+            
+            return ResponseEntity.ok(respuesta);
             
         } catch (Exception e) {
             Map<String, Object> errorResponse = new HashMap<>();
