98aa865e42c6466eae3e1da40773d7684cbb7362
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-29 07:00:18 -0500

    feat: base de los repositorios, cada uno por tabla

---
+++ pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/repository/ConfiguracionGlobalRespository.java
@@ -0,0 +1,9 @@
+package com.pulse_gym.ms_notifications.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pulse_gym.lb_common.entity.notification.ConfiguracionGlobal;
+
+public interface ConfiguracionGlobalRespository extends JpaRepository<ConfiguracionGlobal, Long> {
+    
+}
