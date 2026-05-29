3159f71e8490969bb5201c560e0024c9e4ed9428
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-29 08:55:49 -0500

    PLANTILLACONTROLLER feat: se crea endpoint de leer todas las plentillas

--- pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
+++ pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
@@ -1,10 +1,12 @@
 package com.pulse_gym.ms_notifications.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pulse_gym.lb_common.dto.MessegeGlobalDTO;
 import com.pulse_gym.lb_common.dto.PlantillaNotificacionRequestDTO;
+import com.pulse_gym.lb_common.entity.notification.PlantillaNotificacion;
 import com.pulse_gym.ms_notifications.services.PlantillaService;
 
 import jakarta.validation.Valid;
@@ -54,7 +57,26 @@ public class PlantillaController {
             errorResponse.put("message", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
+    }
 
-
+    @GetMapping("/leer")
+    public ResponseEntity<Map<String, Object>> leerPlantilla(@RequestHeader(value = "X-User-Rol", required = false) String userRol) {
+        try {
+            List<PlantillaNotificacion> notificaciones = plantillaService.leerPlantillas(userRol);            
+            
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "Plantillas encontradas");
+            response.put("count", notificaciones.size());
+            response.put("data", notificaciones);
+            
+            return ResponseEntity.ok(response);
+            
+        } catch (Exception e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
     }
 }
