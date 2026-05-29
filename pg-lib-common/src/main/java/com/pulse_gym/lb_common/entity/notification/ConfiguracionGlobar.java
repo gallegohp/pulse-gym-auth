311074a72075a3256fd216ec197bf2a675e883fe
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-29 06:52:17 -0500

    feat: creacion de la tabla ConfiguracionGlobal

--- pg-lib-common/src/main/java/com/pulse_gym/lb_common/entity/notification/ConfiguracionGlobar.java
+++ pg-lib-common/src/main/java/com/pulse_gym/lb_common/entity/notification/ConfiguracionGlobar.java
@@ -1,5 +1,33 @@
 package com.pulse_gym.lb_common.entity.notification;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "configuracion_global")
 public class ConfiguracionGlobar {
+    
+    /**
+     * Identificador de la configuración global
+     */
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long idConfiguracion;
+
+    /**
+     * Cantidad máxima de notificaciones por día
+     */
+    @Column(name = "max_notificaciones_por_dia", nullable = false)
+    private Long max_notificaciones_por_dia;
+
+    /**
+     * Cantidad máxima de notificaciones por minuto
+     */
+    @Column(name = "max_notificaciones_por_minuto", nullable = false)
+    private Long max_notificaciones_por_minuto;
     
 }
