4dab8fdd581769f925646b493a2476a15efd49e6
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-29 16:58:15 -0500

    PLANTILLACONTROLLER feat: se crea endpoint de activar estado de la plantilla

--- pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
+++ pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/controller/PlantillaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -91,6 +92,25 @@ public class PlantillaController {
     public ResponseEntity<Map<String, Object>> inactivarPlantilla(@PathVariable Long id, @RequestHeader(value = "X-User-Rol", required = false) String userRol) {
         try {
             MessegeGlobalDTO response = plantillaService.inactivarPlantilla(id, userRol);
+            
+            Map<String, Object> respuesta = new HashMap<>();
+            respuesta.put("success", true);
+            respuesta.put("message", response.getMessage());
+            
+            return ResponseEntity.ok(respuesta);
+            
+        } catch (Exception e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+
+    @PutMapping("/activar/{id}")
+    public ResponseEntity<Map<String, Object>> activarPlantilla(@PathVariable Long id, @RequestHeader(value = "X-User-Rol", required = false) String userRol) {
+        try {
+            MessegeGlobalDTO response = plantillaService.activarPlantilla(id, userRol);
             
             Map<String, Object> respuesta = new HashMap<>();
             respuesta.put("success", true);
