d149efe9969e54e4e90503fbfe4399c544cb6386
Camilo Gallego <c.gallego3029@gmail.com>
2026-06-03 20:30:59 -0500

    fix: se modifica el NotificacionRepository, dado Interpretacion mal plantedada del JPA. Se cambia a anotaciones @Query de JPQL

--- pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/repository/NotificacionRepository.java
+++ pg-ms-notifications/src/main/java/com/pulse_gym/ms_notifications/repository/NotificacionRepository.java
@@ -3,20 +3,20 @@ package com.pulse_gym.ms_notifications.repository;
 import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pulse_gym.lb_common.entity.notification.Notificacion;
 
+import feign.Param;
+
 @Repository
 public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
 
-    /**
-     * Cuenta notificaciones enviadas por un usuario desde una fecha
-     */
-    long countById_usuarioAndFechaEnvioAfter(Long idUsuario, LocalDateTime fecha);
+    @Query("SELECT COUNT(n) FROM Notificacion n WHERE n.id_usuario = :idUsuario AND n.fechaEnvio > :fecha")
+    long countById_usuarioAndFechaEnvioAfter(@Param("idUsuario") Long idUsuario, @Param("fecha") LocalDateTime fecha);
 
-    /**
-     * Cuenta notificaciones enviadas por un usuario en un rango de fechas
-     */
-    long countById_usuarioAndFechaEnvioBetween(Long idUsuario, LocalDateTime inicio, LocalDateTime fin);
+    @Query("SELECT COUNT(n) FROM Notificacion n WHERE n.id_usuario = :idUsuario AND n.fechaEnvio BETWEEN :inicio AND :fin")
+    long countById_usuarioAndFechaEnvioBetween(@Param("idUsuario") Long idUsuario,
+            @Param("inicio") LocalDateTime inicio, @Param("fin") LocalDateTime fin);
 }
