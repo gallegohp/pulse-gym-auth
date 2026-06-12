e9c349c968ba4dd1813773fc6758fbf36ed669ca
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-06-12 04:13:10 -0500

    feat: agrego repository

--- pg-ms-users/src/main/java/com/pulse_gym/ms_users/repository/MembresiaRepository.java
+++ pg-ms-users/src/main/java/com/pulse_gym/ms_users/repository/MembresiaRepository.java
@@ -3,12 +3,10 @@ package com.pulse_gym.ms_users.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pulse_gym.lb_common.entity.user.Membresia;
 import com.pulse_gym.lb_common.enums.EnumTipoDuracion;
 
-@Repository
 public interface MembresiaRepository extends JpaRepository<Membresia, Long> {
     
     /**
