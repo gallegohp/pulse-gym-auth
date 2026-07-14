c60fb2b63d141bdb0550308af1094c17664d7d8e
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-14 14:17:07 -0500

    feat: agrego metodo para recopilar los datos  de un socio

--- pg-ms-users/src/main/java/com/pulse_gym/ms_users/service/RutinaIAService.java
+++ pg-ms-users/src/main/java/com/pulse_gym/ms_users/service/RutinaIAService.java
@@ -2,13 +2,22 @@ package com.pulse_gym.ms_users.service;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pulse_gym.lb_common.client.AiClient;
 import com.pulse_gym.lb_common.dto.EstadoMembresiaResponseDTO;
+import com.pulse_gym.lb_common.entity.user.Ejercicio;
+import com.pulse_gym.lb_common.entity.user.HistorialFisico;
+import com.pulse_gym.lb_common.entity.user.PerfilMedico;
+import com.pulse_gym.lb_common.entity.user.RutinaIA;
 import com.pulse_gym.lb_common.entity.user.SocioMembresia;
+import com.pulse_gym.lb_common.entity.user.UsuarioPerfil;
 import com.pulse_gym.ms_users.repository.DetalleRutinaRepository;
 import com.pulse_gym.ms_users.repository.EjercicioRepository;
 import com.pulse_gym.ms_users.repository.HistorialFisicoRepository;
@@ -120,4 +129,92 @@ public class RutinaIAService {
                 .mensaje(membresiaActiva.isActiva() ? "Membresía activa" : "Membresía inactiva")
                 .build();
     }
+
+    public Map<String, Object> recopilarDatosSocio(Long idSocio) {
+        log.info("Recopilando datos del socio ID: {}", idSocio);
+
+        Map<String, Object> datos = new HashMap<>();
+
+        UsuarioPerfil socio = usuarioRepository.findById(idSocio)
+                .orElseThrow(() -> new RuntimeException("Socio no encontrado con ID: " + idSocio));
+
+        datos.put("idSocio", socio.getIdUsuario());
+        datos.put("nombre", socio.getNombre());
+        datos.put("apellido", socio.getApellido());
+        datos.put("email", socio.getEmail());
+        datos.put("edad", calcularEdad(socio.getFechaNacimiento()));
+        datos.put("fechaNacimiento", socio.getFechaNacimiento().toString());
+        datos.put("objetivoPrincipal", socio.getObjetivoPrincipal());
+        datos.put("nivelExperiencia", socio.getNivelExperiencia().name());
+        datos.put("idSede", socio.getIdSede());
+
+        PerfilMedico perfilMedico = perfilMedicoRepository.findBySocio_IdUsuario(idSocio)
+                .orElse(null);
+
+        if (perfilMedico != null) {
+            datos.put("peso", perfilMedico.getPesoKg());
+            datos.put("estatura", perfilMedico.getEstaturaCm());
+            datos.put("alergias", perfilMedico.getAlergias());
+            datos.put("condicionesCronicas", perfilMedico.getCondicionesCronicas());
+            datos.put("lesionesPrevias", perfilMedico.getLesionesPrevias());
+            datos.put("porcentajeGrasa", perfilMedico.getPorcentajeGrasa());
+        } else {
+            log.warn("El socio ID: {} no tiene perfil médico registrado", idSocio);
+        }
+
+        HistorialFisico ultimaMedicion = historialFisicoRepository.findLastMedicionBySocio(idSocio);
+        if (ultimaMedicion != null) {
+            datos.put("pesoKg", ultimaMedicion.getPesoKg());
+            datos.put("porcentajeGrasa", ultimaMedicion.getPorcentajeGrasa());
+            datos.put("porcentajeMusculo", ultimaMedicion.getPorcentajeMusculo());
+            datos.put("fechaMedicion", ultimaMedicion.getFechaMedicion().toString());
+        }
+
+        SocioMembresia membresia = socioMembresiaRepository.findMembresiaActivaBySocio(idSocio)
+                .orElse(null);
+        if (membresia != null) {
+            datos.put("membresiaActiva", true);
+            datos.put("membresiaNombre", membresia.getMembresia().getNombre());
+            datos.put("fechaVencimiento", membresia.getFechaVencimiento().toString());
+        } else {
+            datos.put("membresiaActiva", false);
+        }
+
+        List<RutinaIA> rutinasAnteriores = rutinaRepository.findBySocio_IdUsuarioOrderByFechaGeneracionDesc(idSocio);
+        if (!rutinasAnteriores.isEmpty()) {
+            datos.put("tieneRutinasAnteriores", true);
+            datos.put("cantidadRutinas", rutinasAnteriores.size());
+            datos.put("ultimaRutinaFecha", rutinasAnteriores.get(0).getFechaGeneracion().toString());
+        } else {
+            datos.put("tieneRutinasAnteriores", false);
+        }
+
+        List<Ejercicio> ejercicios = ejercicioRepository.findByActivoTrue();
+        datos.put("ejerciciosDisponibles", ejercicios.stream()
+                .map(this::convertirEjercicioParaIA)
+                .collect(Collectors.toList()));
+
+        Map<String, Object> statsEjercicios = new HashMap<>();
+        statsEjercicios.put("total", (long) ejercicios.size());
+        statsEjercicios.put("porGrupoMuscular", ejercicios.stream()
+                .collect(Collectors.groupingBy(Ejercicio::getGrupoMuscular, Collectors.counting())));
+
+        datos.put("statsEjercicios", statsEjercicios);
+
+        log.info("Datos recopilados para socio ID: {} - {} campos", idSocio, datos.size());
+        return datos;
+    }
+
+    private Map<String, Object> convertirEjercicioParaIA(Ejercicio ejercicio) {
+        Map<String, Object> ej = new HashMap<>();
+        ej.put("id", ejercicio.getIdEjercicio());
+        ej.put("nombre", ejercicio.getNombre());
+        ej.put("grupoMuscular", ejercicio.getGrupoMuscular());
+        ej.put("equipoNecesario", ejercicio.getEquipoNecesario());
+        ej.put("dificultad", ejercicio.getDificultad());
+        ej.put("urlImagen", ejercicio.getUrlImagen());
+        ej.put("urlVideo", ejercicio.getUrlVideo());
+        return ej;
+    }
+
 }
