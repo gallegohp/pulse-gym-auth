17c0e90b8419c829990dad865c0a3d051fbcbd30
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-23 14:16:51 -0500

    feat: agrego el service de de la ia para el utricioanl con los reositorys que vamsoa utiliar

---
+++ pg-ms-users/src/main/java/com/pulse_gym/ms_users/service/PlanNutricionalIAService.java
@@ -0,0 +1,33 @@
+package com.pulse_gym.ms_users.service;
+
+import org.springframework.stereotype.Service;
+
+import com.pulse_gym.ms_users.repository.HistorialFisicoRepository;
+import com.pulse_gym.ms_users.repository.PerfilMedicoRepository;
+import com.pulse_gym.ms_users.repository.RutinaRepository;
+import com.pulse_gym.ms_users.repository.SocioMembresiaRepository;
+import com.pulse_gym.ms_users.repository.UsuarioPerfilRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PlanNutricionalIAService {
+
+    /** Repositorio de usuarios */
+    private final UsuarioPerfilRepository usuarioRepository;
+
+    /** Repositorio de perfiles médicos */
+    private final PerfilMedicoRepository perfilMedicoRepository;
+
+    /** Repositorio de historial físico */
+    private final HistorialFisicoRepository historialFisicoRepository;
+
+    /** Repositorio de rutinas */
+    private final RutinaRepository rutinaRepository;
+
+    /** Repositorio de membresías de socios */
+    private final SocioMembresiaRepository socioMembresiaRepository;
+}
