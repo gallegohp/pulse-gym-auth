10314ff20cfe1d65e4e110283573ad631a20bd89
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-23 17:30:19 -0500

    feat: agrego metodo para recopilar los datos de un socio

--- pg-ms-users/src/main/java/com/pulse_gym/ms_users/service/PlanNutricionalIAService.java
+++ pg-ms-users/src/main/java/com/pulse_gym/ms_users/service/PlanNutricionalIAService.java
@@ -2,9 +2,14 @@ package com.pulse_gym.ms_users.service;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.pulse_gym.lb_common.entity.user.HistorialFisico;
+import com.pulse_gym.lb_common.entity.user.PerfilMedico;
+import com.pulse_gym.lb_common.entity.user.RutinaIA;
 import com.pulse_gym.lb_common.entity.user.SocioMembresia;
 import com.pulse_gym.lb_common.entity.user.UsuarioPerfil;
 import com.pulse_gym.lb_common.enums.EnumRol;
@@ -109,4 +114,65 @@ public class PlanNutricionalIAService {
         throw new SecurityAuthorizationException(
                 "Acceso denegado. Rol '" + userRol + "' no autorizado para generar planes nutricionales");
     }
+
+    /**
+     * 
+     * Recopila todos los datos relevantes del socio para la generación del plan
+     * nutricional
+     * 
+     * @param idSocio ID del socio
+     * @return Mapa con los datos del socio
+     */
+    public Map<String, Object> recopilarDatosSocio(Long idSocio) {
+        log.info("Recopilando datos del socio ID: {}", idSocio);
+
+        Map<String, Object> datos = new HashMap<>();
+
+        UsuarioPerfil socio = usuarioRepository.findById(idSocio)
+                .orElseThrow(() -> new RuntimeException("Socio no encontrado con ID: " + idSocio));
+
+        datos.put("idSocio", socio.getIdUsuario());
+        datos.put("nombre", socio.getNombre());
+        datos.put("apellido", socio.getApellido());
+        datos.put("email", socio.getEmail());
+        datos.put("edad", calcularEdad(socio.getFechaNacimiento()));
+        datos.put("objetivoPrincipal", socio.getObjetivoPrincipal());
+        datos.put("nivelExperiencia", socio.getNivelExperiencia().name());
+
+        PerfilMedico perfilMedico = perfilMedicoRepository.findBySocio_IdUsuario(idSocio)
+                .orElse(null);
+
+        if (perfilMedico != null) {
+            datos.put("peso", perfilMedico.getPesoKg());
+            datos.put("estatura", perfilMedico.getEstaturaCm());
+            datos.put("alergias", perfilMedico.getAlergias());
+            datos.put("condicionesCronicas", perfilMedico.getCondicionesCronicas());
+            datos.put("porcentajeGrasa", perfilMedico.getPorcentajeGrasa());
+        } else {
+            log.warn("El socio ID: {} no tiene perfil médico registrado", idSocio);
+        }
+
+        HistorialFisico ultimaMedicion = historialFisicoRepository.findLastMedicionBySocio(idSocio);
+        if (ultimaMedicion != null) {
+            datos.put("pesoKg", ultimaMedicion.getPesoKg());
+            datos.put("porcentajeGrasa", ultimaMedicion.getPorcentajeGrasa());
+            datos.put("porcentajeMusculo", ultimaMedicion.getPorcentajeMusculo());
+        }
+
+        RutinaIA rutinaActiva = rutinaRepository.findRutinaActivaReciente(idSocio)
+                .orElse(null);
+
+        if (rutinaActiva != null) {
+            datos.put("tieneRutinaActiva", true);
+
+            datos.put("diasEntrenamiento", 3);
+        } else {
+            datos.put("tieneRutinaActiva", false);
+            datos.put("diasEntrenamiento", 0);
+        }
+
+        log.info("Datos recopilados para socio ID: {} - {} campos", idSocio, datos.size());
+        return datos;
+    }
+
 }
